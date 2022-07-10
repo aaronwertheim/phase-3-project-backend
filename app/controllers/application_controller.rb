02d199e02646051cd622aaf1c6bfd29c764d1cd8
justinmramon <justinmramon@gmail.com>
2022-07-10 17:45:40 -0500

    updated app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,6 +11,17 @@ class ApplicationController < Sinatra::Base
     movies.to_json
   end
 
+  get "/movies/:id" do
+    movie = Movie.find(params[:id])
+    movie.to_json
+  end
+
+  delete "/movies/:id" do
+    movie = Movie.find(params[:id])
+    movie.destroy
+    movie.to_json
+  end
+
   get "/users" do
     users = User.all
     users.to_json
